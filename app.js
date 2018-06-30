766fd7a650bca7bffcef77f14e6fe4b9c7496e89
zack403 <34305980+zack403@users.noreply.github.com>
2018-06-30 10:07:48 +0100

    update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ const convertCurrency = (amount, fromCurrency, toCurrency, cb) => {
     validate = document.getElementById('amount').value;
     if (isNaN(validate) || validate === "") {
         document.getElementById("amount").style.border = "1px solid red";
-        document.getElementById("amount").value = "Enter amount";
+        
     }
     else {
         let convert = "converting..."
